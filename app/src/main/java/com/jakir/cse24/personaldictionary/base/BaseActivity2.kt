ea19301d420800f0cdea43d93857189996e5100c
Md Jakir Hossain <jakir.cse24@gmail.com>
2023-11-21 17:06:11 +0600

    udpated .gitignore

---
+++ app/src/main/java/com/jakir/cse24/personaldictionary/base/BaseActivity2.kt
@@ -0,0 +1,213 @@
+package com.jakir.cse24.personaldictionary.base
+
+import android.app.Activity
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.util.Patterns
+import android.util.TypedValue
+import android.view.*
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+import com.jakir.cse24.personaldictionary.R
+import com.jakir.cse24.personaldictionary.data.PreferenceManager
+
+
+abstract class BaseActivity2<VB : ViewBinding> : AppCompatActivity() {
+    lateinit var binding: VB
+    private var dialog: AlertDialog? = null
+    companion object{
+        var isLanguageChange = false
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(getSelectedTheme())
+//        makeStatusBarTransparent()
+        binding = getViewBinding()
+        onViewReady(savedInstanceState)
+    }
+
+    private fun getSelectedTheme():Int{
+        return when(PreferenceManager.currentTheme){
+            "Light"-> R.style.lightTheme
+            "Dark"-> R.style.darkTheme
+            "Classic"-> R.style.classicTheme
+            else -> R.style.darkTheme
+        }
+    }
+
+    /**
+     * This abstract method is for getting layout resource ID from every activity which extend BaseActivity
+     * Created by Md. Jakir Hossain on 03/11/2019.
+     *
+     * @return layout resource ID
+     */
+    abstract fun getViewBinding(): VB
+
+    /**
+     * This method is for every child activities of BaseActivity and can do what they are want to do in onCreate().
+     * Created by Md. Jakir Hossain on 03/11/2019.
+     *
+     * @param savedInstanceState instance of Bundle of OnCreate.
+     */
+    abstract fun onViewReady(savedInstanceState: Bundle?)
+
+    /**
+     * This method is for printing a ERROR log message on the system console
+     * Created by Md. Jakir Hossain on 02/05/2019.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
+     * @param msg The message you would like logged.
+     */
+    protected fun logE(tag: String, msg: String) {
+        Log.e(tag, msg)
+    }
+
+    /**
+     * This method is for printing a DEBUG log message on the system console
+     * Created by Md. Jakir Hossain on 02/05/2019.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
+     * @param msg The message you would like logged.
+     */
+    protected fun logD(tag: String, msg: String) {
+        Log.d(tag, msg)
+    }
+
+    /**
+     * This method is for showing message using android {@link Toast}
+     * Created by Md. Jakir Hossain on 29/04/2019.
+     *
+     * @param msg message which will display on screen
+     */
+    protected fun showToast(msg: String) {
+        val toast: Toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()
+    }
+
+    /**
+     * This method is for showing error/warning using alert dialog.
+     * Created by Md. Jakir Hossain on 29/04/2019.
+     *
+     * @param title alert title
+     * @param msg   alert message
+     * @param icon  alert icon
+     */
+    protected fun showAlert(title: String, msg: String, icon: Int) {
+        val dialog: AlertDialog.Builder = AlertDialog.Builder(this)
+        dialog.setTitle(title)
+        dialog.setIcon(icon)
+        dialog.setMessage(msg)
+        dialog.setPositiveButton(
+            getString(android.R.string.ok)
+        ) { dialogInterface, _ ->
+            dialogInterface.dismiss()
+        }
+        dialog.show()
+    }
+
+    /**
+     * This method is for showing error/warning using alert dialog.
+     * Created by Md. Jakir Hossain on 29/04/2019.
+     * <p>
+     * {@link BaseActivity#showAlert(String, String, int)}  supper method}
+     *
+     * @param msg error/warning message
+     */
+    protected fun showAlert(msg: String) {
+        showAlert("Dictionary", msg, android.R.drawable.ic_dialog_alert)
+    }
+
+    /**
+     * This method is for showing progress with custom message(optional).
+     * Created by Md. Jakir Hossain on 07/11/2019.
+     *
+     * @param msg progress message(optional)
+     */
+    protected fun showProgressDialog(msg: String = "") {
+        val view: View = LayoutInflater.from(this).inflate(R.layout.layout_progress_dialog, null)
+        val alert = AlertDialog.Builder(this)
+        alert.setCancelable(false)
+        alert.setView(view)
+        if (dialog != null) {
+            dialog!!.dismiss()
+        }
+        dialog = alert.create()
+        if (TextUtils.isEmpty(msg)) {
+            view.findViewById<TextView>(R.id.tvProgressMessage).visibility = View.GONE
+            dialog!!.getWindow()?.setBackgroundDrawableResource(android.R.color.transparent)
+        } else
+            view.findViewById<TextView>(R.id.tvProgressMessage).text = msg
+
+        dialog!!.show()
+    }
+
+    /**
+     * This method is for hide/clear progress dialog
+     * Created by Md. Jakir Hossain on 07/11/2019.
+     */
+    protected fun hideProgressDialog() {
+        if (dialog != null) {
+            dialog!!.dismiss()
+            dialog = null
+        }
+    }
+
+
+    /**
+     * This function is for setting actionbar title
+     * Created by Md. Jakir Hossain on 03/11/2019.
+     * @param title action bar title
+     */
+    protected fun settingActionBar(title: String, status: Boolean) {
+        supportActionBar?.setHomeButtonEnabled(status)
+        supportActionBar?.setDisplayHomeAsUpEnabled(status)
+        supportActionBar?.title = title
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun isEmailValid(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.toRegex().matches(email)
+    }
+
+    @ColorInt
+    fun Context.getColorFromAttr(
+        @AttrRes attrColor: Int,
+        typedValue: TypedValue = TypedValue(),
+        resolveRefs: Boolean = true
+    ): Int {
+        theme.resolveAttribute(attrColor, typedValue, resolveRefs)
+        return typedValue.data
+    }
+
+    protected fun Activity.makeStatusBarTransparent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.apply {
+                clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+                addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    decorView.systemUiVisibility =
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    statusBarColor = getColorFromAttr(R.attr.pageBgColor)
+                } else {
+                    decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                }
+                statusBarColor = getColorFromAttr(R.attr.pageBgColor)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/jakir/cse24/personaldictionary/base/BaseFragment2.kt
@@ -0,0 +1,174 @@
+package com.jakir.cse24.personaldictionary.base
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.*
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.viewbinding.ViewBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+import com.jakir.cse24.personaldictionary.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Created by Md. Jakir Hossain on 02/05/2019.
+ */
+abstract class BaseFragment2<VB : ViewBinding> : Fragment() {
+
+    abstract fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?): VB
+    protected lateinit var binding: VB
+
+
+    abstract fun onViewReady(savedInstanceState: Bundle?, view: View)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = getViewBinding(inflater,container)
+        onViewReady(savedInstanceState, binding.root)
+        return binding.root
+    }
+
+    private var dialog: AlertDialog? = null
+
+    /**
+     * This method is for printing a ERROR log message on the system console
+     * Created by Md. Jakir Hossain on 02/05/2019.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
+     * @param msg The message you would like logged.
+     */
+    protected fun logE(tag: String, msg: String) {
+        Log.e(tag, msg)
+    }
+
+    /**
+     * This method is for printing a DEBUG log message on the system console
+     * Created by Md. Jakir Hossain on 02/05/2019.
+     *
+     * @param tag Used to identify the source of a log message. It usually identifies the class or activity where the log call occurs.
+     * @param msg The message you would like logged.
+     */
+    protected fun logD(tag: String, msg: String) {
+        Log.d(tag, msg)
+    }
+
+    /**
+     * This method is for showing message using android {@link Toast}
+     * Created by Md. Jakir Hossain on 29/04/2019.
+     *
+     * @param msg message which will display on screen
+     */
+    protected fun showToast(msg: String) {
+        val toast: Toast = Toast.makeText(context, msg, Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()
+    }
+
+    protected fun showSnack(msg: String) {
+        val snack = Snackbar.make(this.view!!, msg, Snackbar.LENGTH_LONG)
+        snack.show()
+    }
+
+    /**
+     * This method is for showing error/warning using alert dialog.
+     * Created by Md. Jakir Hossain on 29/04/2019.
+     *
+     * @param title alert title
+     * @param msg   alert message
+     * @param icon  alert icon
+     */
+    protected fun showAlert(title: String, msg: String, icon: Int) {
+        val dialog: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        dialog.setTitle(title)
+        dialog.setIcon(icon)
+        dialog.setMessage(msg)
+        dialog.setPositiveButton(
+            getString(android.R.string.ok)
+        ) { dialogInterface, _ ->
+            dialogInterface.dismiss()
+        }
+        dialog.show()
+    }
+
+    protected fun showAlertWithChoice(
+        title: String,
+        msg: String,
+        icon: Int
+    ): MutableLiveData<Boolean> {
+        val response = MutableLiveData<Boolean>()
+        val dialog = MaterialAlertDialogBuilder(requireContext())
+        dialog.setTitle(title)
+        dialog.setMessage(msg)
+        dialog.setPositiveButton(
+            getString(android.R.string.ok)
+        ) { dialogInterface, _ ->
+            response.value = true
+            dialogInterface.dismiss()
+        }
+        dialog.setNegativeButton(getString(android.R.string.cancel)) { dialogInterface, i ->
+            response.value = false
+            dialogInterface.dismiss()
+        }
+        dialog.show()
+
+        return response
+    }
+
+    /**
+     * This method is for showing error/warning using alert dialog.
+     * Created by Md. Jakir Hossain on 29/04/2019.
+     * <p>
+     * {@link BaseActivity#showAlert(String, String, int)}  supper method}
+     *
+     * @param msg error/warning message
+     */
+    protected fun showAlert(msg: String) {
+        showAlert("Dictionary", msg, android.R.drawable.ic_dialog_alert)
+    }
+
+    /**
+     * This method is for showing progress with custom message(optional).
+     * Created by Md. Jakir Hossain on 07/11/2019.
+     *
+     * @param msg progress message(optional)
+     */
+    protected fun showProgressDialog(msg: String = "") {
+        val view: View = LayoutInflater.from(context).inflate(R.layout.layout_progress_dialog, null)
+        val alert = AlertDialog.Builder(requireContext())
+        alert.setCancelable(false)
+        alert.setView(view)
+        if (dialog != null) {
+            dialog!!.dismiss()
+        }
+        dialog = alert.create()
+        if (TextUtils.isEmpty(msg)) {
+            view.findViewById<TextView>(R.id.tvProgressMessage).visibility = View.GONE
+            dialog!!.getWindow()?.setBackgroundDrawableResource(android.R.color.transparent)
+        } else
+            view.findViewById<TextView>(R.id.tvProgressMessage).text = msg
+
+        dialog!!.show()
+    }
+
+    /**
+     * This method is for hide/clear progress dialog
+     * Created by Md. Jakir Hossain on 07/11/2019.
+     */
+    protected fun hideProgressDialog() {
+        if (dialog != null) {
+            dialog!!.dismiss()
+            dialog = null
+        }
+    }
+
+}
